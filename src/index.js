5bba6cda02cf71ce6e6fb69b45e583e1c14f0208
Ronen Nachmias <ronen.hamias@gmail.com>
2019-12-26 17:25:50 +0200

    fix sha

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ if(!process.env.GITHUB_TOKEN) console.log("required GITHUB_TOKEN enviroment vari
 if(!process.env.GITHUB_SECRET) console.log("required GITHUB_SECRET enviroment variable is missing" );
 if(!process.env.PUBLIC_API_PORT) console.log("required GITHUB_API_PORT enviroment variable is missing" );
 if(!process.env.INTERNAL_API_PORT) console.log("required STATUS_API_PORT enviroment variable is missing" );
-if(!process.env.YOUR_SERVER_URL) console.log("required YOUR_SERVER_URL enviroment variable is missing" );
 if(!process.env.MONGO_DB_CONNECTION_STRING) console.log("required MONGO_DB_CONNECTION_STRING enviroment variable is missing" );
-if(!process.env.MONGO_DB_DATABASE_NAME) console.log("required MONGO_DB_DATABASE_NAME enviroment variable is missing" );
+
+console.log("GITHUB_TOKEN: " + process.env.GITHUB_TOKEN);
+console.log("GITHUB_SECRET: " + process.env.GITHUB_TOKEN);
+console.log("PUBLIC_API_PORT: " + process.env.GITHUB_TOKEN);
+console.log("INTERNAL_API_PORT: " + process.env.GITHUB_TOKEN);
+console.log("MONGO_DB_CONNECTION_STRING: " + process.env.GITHUB_TOKEN);
 
 const gateway = require("./http-gateway");
