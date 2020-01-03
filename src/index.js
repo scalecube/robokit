80547a6d3bd721b539e9c9f960623348966d8671
Ronen Nachmias <ronen.hamias@gmail.com>
2020-01-03 16:08:18 +0200

    add mask to secret

--- src/index.js
+++ src/index.js
@@ -6,7 +6,30 @@ if(!process.env.PUBLIC_API_PORT) console.log("required GITHUB_API_PORT enviromen
 if(!process.env.INTERNAL_API_PORT) console.log("required STATUS_API_PORT enviroment variable is missing" );
 if(!process.env.MONGO_DB_CONNECTION_STRING) console.log("required MONGO_DB_CONNECTION_STRING enviroment variable is missing" );
 
-console.log("GITHUB_TOKEN: " + process.env.GITHUB_TOKEN);
+/**
+ * @param {string} str
+ * @param {string} maskChar
+ * @param {number} unmaskedLength
+ * @param {boolean} [maskFromStart]
+ * @returns {string}
+ */
+function mask(str, maskChar, unmaskedLength, maskFromStart = true) {
+    const maskStart = maskFromStart ? 0 : Math.max(0, unmaskedLength);
+    const maskEnd = maskFromStart ? Math.max(0, str.length - unmaskedLength) : str.length;
+    return str
+        .split("")
+        .map((char, index) => {
+            if (index >= maskStart && index < maskEnd) {
+                return maskChar;
+            }
+            else {
+                return char;
+            }
+        })
+        .join("");
+}
+
+console.log("GITHUB_TOKEN: " + mask(process.env.GITHUB_TOKEN,"*",16 ));
 console.log("GITHUB_SECRET: " + process.env.GITHUB_SECRET);
 console.log("PUBLIC_API_PORT: " + process.env.PUBLIC_API_PORT);
 console.log("INTERNAL_API_PORT: " + process.env.INTERNAL_API_PORT);
