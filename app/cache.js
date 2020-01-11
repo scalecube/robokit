6b302ccbf680d1c3cd084938d4396d6ab404974f
Ronen Nachmias <ronen.hamias@gmail.com>
2020-01-11 15:23:12 +0200

    fix test

--- app/cache.js
+++ app/cache.js
@@ -1,6 +1,22 @@
+const createScheduler = require('probot-scheduler');
 const contexts = new Map();
 
 class Cache {
+    constructor(app){
+        createScheduler(app, {
+            delay: false, // delay is enabled on first run
+            interval: 15 * 60 * 1000 // 15 minutes
+        });
+
+        app.on('schedule.repository', context => {
+            this.set(context.payload.repository.owner.login , context.payload.repository.name ,context.github);
+        });
+
+        app.on('*', async context => {
+            console.log("EVENT NAME: " + context.name);
+            this.set(context.payload.repository.owner.login , context.payload.repository.name ,context.github);
+        });
+    }
     set(owner,repo, ctx) {
         contexts.set(owner+ "/" + repo, ctx);
     }
--- index.js
+++ index.js
@@ -1,33 +1,15 @@
-const createScheduler = require('probot-scheduler');
 const ApiGateway = require('./app/http-gateway');
 const Cache = require('./app/cache');
-const cache = new Cache();
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-
-  createScheduler(app, {
-    delay: false, // delay is enabled on first run
-    interval: 1000 * 60 * 5 // 1 day
-  });
-
-  app.on('schedule.repository', context => {
-    cache.set(context.payload.repository.owner.login , context.payload.repository.name ,context.github);
-  });
-
-  app.on('*', async context => {
-    cache.set(context.payload.repository.owner.login , context.payload.repository.name ,context.github);
-  });
+  const cache = new Cache(app);
 
   app.log('Starting the T+Bot service.');
   const api = new ApiGateway(app,cache);
-
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
-    return context.github.issues.createComment(issueComment);
-  });
 
   app.on('check_suite', async context => {
     return api.onCheckSuite(context);
