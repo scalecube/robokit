ef0c5e9644eb7088682c53343049f81459e85a0e
Ronen Nachmias <ronen.hamias@gmail.com>
2020-01-27 13:38:17 +0200

    fix test

--- app/config.js
+++ app/config.js
@@ -1,15 +1,30 @@
 module.exports = {
     deploy : {
-        name: "Robo-Kit Deploy",
-        on: [
-            'Travis CI - Pull Request',
-            'trigger_deploy'
-        ],
-        label: {
-            name: "DEPLOY"
+        check:{
+            name: "Robo-Kit Deploy",
+            queued: {
+                title: "Deploy is Waiting for ci to complete.",
+                summary: "deploy will start when check suite completes",
+                text: "waiting for CI to complete successfully"
+            },
+            in_progress: {
+                title: "Robo-kit is Deploying branch: ",
+                summary: "Triggered a Continues-Deployment pipeline",
+                text: "Waiting for Continues deployment status updates"
+            }
+        },
+        on: {
+            actions:[
+                'Travis CI - Pull Request',
+                'trigger_deploy'
+            ],
+            pull_request: {
+                labeled: ["DEPLOY"]
+            }
         }
     },
     labels:[{
         name: "DEPLOY"
     }]
 }
+
