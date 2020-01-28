40bd2eca92ef4251e4b2ad19fe8f9ba6ccf2e3a6
Ronen Nachmias <ronen.hamias@gmail.com>
2020-01-28 17:30:37 +0200

    fix test

--- app/github/webhooks-router.js
+++ app/github/webhooks-router.js
@@ -16,8 +16,8 @@ class WebhooksRouter {
     this.routes.forEach(route => {
       if (route.owner && route.owner === owner) {
         if (route.repo === repo || !route.repo) {
+
           for (let [key, value] of Object.entries(ctx)) {
-            console.log(`${key}: ${value}`);
             route.url = route.url.replace("${"+ key + "}", value);
           }
           httpClient.post(route.url, ctx).then((msg) => {
