fd36739cc82d320c3aaecc1396cd811da62aca11
Ronen Nachmias <ronen.hamias@gmail.com>
2020-01-28 18:36:19 +0200

    fix test

--- app/http-gateway.js
+++ app/http-gateway.js
@@ -283,7 +283,7 @@ class ApiGateway {
       }
     } else {
       for (let i = 0; i < cfg.deploy.on.push.actions.length; i++) {
-        if ((deploy.checkName == cfg.deploy.on.push.actions[i]) && (deploy.status == status)) {
+        if ((deploy.check_run_name == cfg.deploy.on.push.actions[i]) && (deploy.status == status)) {
           return true;
         }
       }
