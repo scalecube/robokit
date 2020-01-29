4295bf57a5d1e073a01a5213ef5114c18f9ca266
Ronen Nachmias <ronen.hamias@gmail.com>
2020-01-29 12:15:04 +0200

    fix test

--- app/http-gateway.js
+++ app/http-gateway.js
@@ -207,6 +207,7 @@ class ApiGateway {
     return false;
   }
 
+
   async onCheckRun(context) {
     console.log(context.payload.check_run.name + " - " + context.payload.check_run.status + " - " + context.payload.check_run.conclusion);
     let deploy = await this.deployContext(context);
