a1b284d62a9e3502fc961f75d7a93b0e06281742
Ronen Nachmias <ronen.hamias@gmail.com>
2020-02-23 22:30:32 +0200

    format status messages

--- app/spinnaker/spinnaker-client.js
+++ app/spinnaker/spinnaker-client.js
@@ -63,7 +63,8 @@ class SpinnakerAPI {
         method: 'get',
         url: url,
         headers: {
-          cookie: 'SESSION=' + this.session
+          cookie: 'SESSION=' + this.session,
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
         }
       }).then((res) => {
         resolve(res.data)
