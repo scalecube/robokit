5b00ddadc37d9766022e8291af0c917102577633
Ronen Nachmias <ronen.hamias@gmail.com>
2020-03-06 14:16:10 +0200

    format status messages

--- app/statuses/templates.js
+++ app/statuses/templates.js
@@ -5,15 +5,20 @@ class Templates {
 
   constructor (){
     this.statusContent = new Map();
-    this.statusContent.set('running',  fs.readFileSync(path.resolve(__dirname,'./running.md'),"utf8"))
-    this.statusContent.set('starting', fs.readFileSync(path.resolve(__dirname,'./starting.md'),"utf8"))
-    this.statusContent.set('waiting',  fs.readFileSync(path.resolve(__dirname,'./waiting.md'),"utf8"))
-    this.statusContent.set('canceled', fs.readFileSync(path.resolve(__dirname,'./canceled.md'),"utf8"))
-    this.statusContent.set('status',   fs.readFileSync(path.resolve(__dirname,'./status.md'),"utf8")
-      .replace('${GRAPHANA_URL}',process.env.GRAPHANA_URL))
+    this.statusContent.set('running',  this.env(fs.readFileSync(path.resolve(__dirname,'./running.md'),"utf8")))
+    this.statusContent.set('starting', this.env(fs.readFileSync(path.resolve(__dirname,'./starting.md'),"utf8")))
+    this.statusContent.set('waiting',  this.env(fs.readFileSync(path.resolve(__dirname,'./waiting.md'),"utf8")))
+    this.statusContent.set('canceled', this.env(fs.readFileSync(path.resolve(__dirname,'./canceled.md'),"utf8")))
+    this.statusContent.set('status',   this.env(fs.readFileSync(path.resolve(__dirname,'./status.md'),"utf8")))
   }
+
   get(key) {
     return this.statusContent.get(key);
+  }
+
+  env(data) {
+    data = data.replace('${GRAPHANA_URL}',process.env.GRAPHANA_URL)
+    return data
   }
 }
 
