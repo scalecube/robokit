3e493526149b008b8084b659286c7ae99d997d76
Ronen Nachmias <ronen.hamias@gmail.com>
2020-03-06 14:02:45 +0200

    format status messages

--- app/statuses/templates.js
+++ app/statuses/templates.js
@@ -9,7 +9,8 @@ class Templates {
     this.statusContent.set('starting', fs.readFileSync(path.resolve(__dirname,'./starting.md'),"utf8"))
     this.statusContent.set('waiting',  fs.readFileSync(path.resolve(__dirname,'./waiting.md'),"utf8"))
     this.statusContent.set('canceled', fs.readFileSync(path.resolve(__dirname,'./canceled.md'),"utf8"))
-    this.statusContent.set('status',   fs.readFileSync(path.resolve(__dirname,'./status.md'),"utf8"))
+    this.statusContent.set('status',   fs.readFileSync(path.resolve(__dirname,'./status.md'),"utf8")
+      .replace('${GRAPHANA_URL}',process.env.GRAPHANA_URL))
   }
   get(key) {
     return this.statusContent.get(key);
