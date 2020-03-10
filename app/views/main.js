50ff9eb493e9793be88ee88ba881aa40584c4525
Ronen Nachmias <ronen.hamias@gmail.com>
2020-03-10 08:25:45 +0200

    rename github-gateway to robokit

---
+++ app/views/main.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import MUIDataTable from "mui-datatables";
+import createReactSettings from './plotly/components/settings/app'
+
+createReactSettings('root')
