cf48bccaafed8e5ff7e6f5baad21f50742c630be
Ronen Nachmias <ronen.hamias@gmail.com>
2020-04-18 16:06:23 +0300

    fix spinless url

--- app/pipelines/stream.js
+++ app/pipelines/stream.js
@@ -1,13 +1,21 @@
-const axios = require('axios')
 const readline = require('readline')
 var http = require('http')
+var https = require('https')
 
+function provider (url) {
+  if (!url.startsWith('https')) {
+    return http
+  } else {
+    return https
+  }
+}
 // GET request for remote imag
 class Stream {
   static from (url) {
+    const protocol = provider(url)
     return {
       on: (data, end) => {
-        http.get(url, (response) => {
+        protocol.get(url, (response) => {
           readline.createInterface({
             input: response
           }).on('line', data).on('close', end)
